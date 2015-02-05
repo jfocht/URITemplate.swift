0c53ae27e007538a3b22bb4f164d38ee0f826222
Jordan Focht <jfocht@gmail.com>
2015-02-05 09:15:09 -0600

    add iOS target

---
+++ URITemplate-iOS/URITemplate-iOS.h
@@ -0,0 +1,19 @@
+//
+//  URITemplate-iOS.h
+//  URITemplate-iOS
+//
+//  Created by Jordan Focht on 2/5/15.
+//  Copyright (c) 2015 Kyle Fuller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for URITemplate-iOS.
+FOUNDATION_EXPORT double URITemplate_iOSVersionNumber;
+
+//! Project version string for URITemplate-iOS.
+FOUNDATION_EXPORT const unsigned char URITemplate_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URITemplate_iOS/PublicHeader.h>
+
+
---
+++ URITemplate-iOSTests/URITemplate_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  URITemplate_iOSTests.swift
+//  URITemplate-iOSTests
+//
+//  Created by Jordan Focht on 2/5/15.
+//  Copyright (c) 2015 Kyle Fuller. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class URITemplate_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
